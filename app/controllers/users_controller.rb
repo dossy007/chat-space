78de3aa1aba952d53284e98b8049450b5f2e8571
dossy007 <toshi@toshinoMacBook-ea.local>
2018-08-28 13:51:49 +0900

    add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,17 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def update
+  def update(create_params)
+  	if current_user.update(create_params)
+  		redirect_to root_path
+  	else
+  		render :edit
+  	end
+  end
+
+  private
+
+  def create_params
+  	params.require(:user).permit(:name,:email)
   end
 end
