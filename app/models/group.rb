76c6a51e6f9764ea6f9f8b1536064461c253f97e
dossy007 <toshi@toshinoMacBook-ea.local>
2018-09-10 14:07:33 +0900

    Fix conflict

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,13 +1,5 @@
 class Group < ApplicationRecord
 	has_many :members
-<<<<<<< HEAD
 	has_many :users, through: :members
-=======
-<<<<<<< HEAD
-	has_many :members, through: :members
-=======
-	has_many :users, through: :members
->>>>>>> origin/master
->>>>>>> master
 	validates :name, presence: true
 end
