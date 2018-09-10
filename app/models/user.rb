6ed7255f690a7cd7ec7dd8c64bef77c82dbab587
dossy007 <toshi@toshinoMacBook-ea.local>
2018-09-10 14:06:27 +0900

    Edit indent

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-=======
-
->>>>>>> master
   has_many :messages
   has_many :members
   has_many :groups, through: :members
